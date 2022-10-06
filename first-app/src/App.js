00fc3978d8ea2cc8fdfa2d6c393872818591566b
migyeonglee <ak10044@naver.com>
2022-10-06 14:37:52 +0900

    component

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,7 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import img from './img/pngwing.com (2).png';
+// import img from './img/pngwing.com (2).png';
+// import FunctionComponent from './functionComponent';
+// import ClassComponent from './classComponent';
+import Test from './test';
+import Test2 from './test2';
 function App() {
+    return (
+        <>
+      <Test />
+      <Test2 />
+     
+</>     )
+}
     // const name = "이미경";
     // let classs ="";
 // if(name === "이미경"){
@@ -17,85 +28,89 @@ function App() {
 //     backgroundColor : "skyblue",
 //     marginBottom : "30px",
 // }
-let name = "로이";
-let animal = "강아지"
-let text = {
-    textDecoration: "underline",
-    display :"inline"
+// let name = "로이";
+// let animal = "강아지"
+// let text = {
+//     textDecoration: "underline",
+//     display :"inline"
 
-}
-let a = 3;
-let b = 1;
-let memo="";
-if(a > b ){
-memo = "a가 b보다 큽니다";
-}else if(a < b && a ===b){
-    memo = "a가 b보다 작거나 같습니다";
+// }
+// let a = 3;
+// let b = 1;
 
-}
-let login = {
-    textAlign: "center",
-    marginBottom : "10px"
-}
-let title = "Hello World";
-  return (
-   <>
-    {/* <h1>안녕</h1>
-    <h2>{name === "이미경" ? "kdt2기": "누구"}</h2> */}
-{/* <div className='App-header' style={styles}>안녕</div>
-<div style={{
-    color : "blue",
-    width : "100px",
-    height : "100px",
-    backgroundColor : "red",
-    }}>안녕</div>
-<hr />
-<br />
-<form action="">
-<input type="text" /><br />
-</form> */}
-<div>이것은 div입니다.</div>
-<div><h3>이것은 div 안에 있는 h3태그 입니다</h3></div>
+// let login = {
+//     textAlign: "center",
+//     marginBottom : "10px"
+// }
+// let title = "Hello World";
+//   return (
+//    <>
+//    <FunctionComponent />
+// <div>kdt 2기</div>
 
-<h2>제 반려 동물의 이름은 <p style={text}>{name}</p>입니다.</h2>
-<h2><p style={text}>{name}</p>는 <p style={text}>{animal}</p>입니다.</h2>
+/*
+   {/* 아래 xml */
+//  <h1>안녕</h1>
+//     <h2>{name === "이미경" ? "kdt2기": "누구"}</h2> 
+//  <div className='App-header' style={styles}>안녕</div> */}
+// <div style={{
+//     color : "blue",
+//     width : "100px",
+//     height : "100px",
+//     backgroundColor : "red",
+//     }}>안녕</div>
+// <hr />
+// <br />
+// <form action="">
+// <input type="text" /><br />
+// </form> 
 
-<div>{3+5==8 ? "정답입니다!" : "오답입니다!"}</div>
-<br />
-<div>{a>b && "a가 b보다 큽니다"}</div>
-<div className='test'>{title}</div>
-<div style={login}>아이디 : <input className='input' type="text" /></div>
-<div style={login}>비밀번호 : <input className='input' type="text" /></div>
+ // <div>이것은 div입니다.</div>
+// <div><h3>이것은 div 안에 있는 h3태그 입니다</h3></div>
 
-<div className='a'>
-<div className='red'></div>
-<div className='orange'></div>
-<div className='yellow'></div>
-<div className='green'></div>
-<div className='blue'></div>
-<div className='navy'></div>
-<div className='purple'></div>
-</div>
-<br />
-<br />
-<div className='q red'>
-    <div className='eye'>
-        <div className='mini'></div>
-    </div>
-    <div className='w orange'>
-        <div className='e yellow'>
-            <div className='r green'>
-                <div className='t blue'>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<img className='img' src={img} alt="" />
-<div></div>
-<br />
-   </>
-  );
-}
+// <h2>제 반려 동물의 이름은 <p style={text}>{name}</p>입니다.</h2>
+// <h2><p style={text}>{name}</p>는 <p style={text}>{animal}</p>입니다.</h2>
+
+// <div>{3+5===8 ? "정답입니다!" : "오답입니다!"}</div>
+// <br />
+// <div>{a>b && "a가 b보다 큽니다"}</div>
+// <div className='test'>{title}</div>
+// <div style={login}>아이디 : <input className='input' type="text" /></div>
+// <div style={login}>비밀번호 : <input className='input' type="text" /></div>
+
+// <div className='a'>
+// <div className='red'></div>
+// <div className='orange'></div>
+// <div className='yellow'></div>
+// <div className='green'></div>
+// <div className='blue'></div>
+// <div className='navy'></div>
+// <div className='purple'></div>
+// </div>
+// <br />
+// <br />
+// <div className='q red'>
+//     <div className='eye'>
+//         <div className='mini'></div>
+//     </div>
+//     <div className='w orange'>
+//         <div className='e yellow'>
+//             <div className='r green'>
+//                 <div className='t blue'>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+// </div>
+// <img className='img' src={img} alt="" />
+// <div></div>
+// <br /> 
+//    </>
+//   );
+
+
+// 
 
 export default App;
+// export default test;
+// export default test2;
---
+++ first-app/src/functionComponent.js
@@ -0,0 +1,17 @@
+function FunctionComponent(){
+return (
+    <>
+        <div>안녕</div>
+    </>
+    );
+}
+export default FunctionComponent;
+// const FunctionComponent = () => {
+    // return (
+        //     <>
+        //         <div></div>
+        //      </>
+        // )
+        // }
+        
+// export default FunctionComponent;
