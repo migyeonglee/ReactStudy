cd716509af43191dc1b655cb2069d31187532229
migyeonglee <ak10044@naver.com>
2022-10-11 14:47:15 +0900

    map study

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
-// import ClassState from './class';
-// import FunctionState from './function';
-// import Map from './map';
+import Map from './map';
+// import { useState } from 'react';
 function App() {
-   let list = ["a","b","c","d","e"];
-  //  items => 배열이 담김
-   let items = list.map((value , id,arr) => {
-    console.log("value : ", value);
-    console.log("id : ", id);
-    console.log("arr : ", arr);
-    return value + id;
-   })
-console.log('items : ',items)
-  //  items = ['a0','b1','c2','d3','e4']
+
 
   return (
    <>
-
+   <Map></Map>
+    
    </>
   );
 }
--- first-app/src/map.js
+++ first-app/src/map.js
@@ -1,20 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useState } from 'react';
 
-class Map extends Component {
-    
-    render() {
-        return (
-            <>
-                <h1>map 메소드</h1>
-                <p>배열에서 사용할 수 있음</p>
-                <hr />
-            </>
-        );
-    }
-}
+const Map = () => {
+      //  let list = ["a","b","c","d","e"];
+  //  items => 배열이 담김
+  //  let items = list.map((value, id, arr) => {
+//     console.log("value : ", value);
+//     console.log("id : ", id);
+//     console.log("arr : ", arr);
+//     return value + id;
+  //  })
+   
+// console.log('items : ',items)
+  //  items = ['a0','b1','c2','d3','e4']
+/* 
+value : list를 순서대로 돌며 나오는 값
+id : 방금 나온 값의 인덱스
+arr : 현재 반복을 돌고 있는 배열
+items : "return value + id;"로 만들어진 배열
+*/
+    // let list = ["a","b","c","d","e"];
+let listTemp =[
+    {id :1, value : "a"},
+    {id :2, value : "b"},
+    {id :3, value : "c"},
+    {id :4, value : "d"},
+    {id :5, value : "e"},
+];
+    let [list, setList]=useState(listTemp);
+    let [inputValue, setInput]=useState("");
+    // let newList = list.concat({id : 6, value : inputValue});
+    // setList(newList);
+    return (
+        <>
+        {/* <ul> */}
+      {/* <li>a</li>
+      <li>b</li>
+      <li>c</li>
+      <li>d</li>
+      <li>e</li> */}
+      {/* 위와 아래가 동일하게 나옴 , 콘솔에 error 뜨면 key값 지정해줄것 */}
+      {/* map() 함수를이용해컴포넌트를생성할때“key”사용을권장한다.
+        Why? React는 자율적으로 업데이트전 기존요소와 업데이트요소를 비교하는데 key를 사용한다 */}
+      {/* {list.map((value, id) => {return <li key = {id}>{value}</li>})} */}
+      {/* <Map></Map> */}
+   {/* </ul> */}
+        <input type="text " onChange={(e)=> {setInput(e.target.value);}} />
+        <button onClick={()=>{
+            // id가 6인 곳에 추가 
+            //  let newList = list.concat({id : 6, value : inputValue});
+             let newList = list.concat({id : list.length+1, value : inputValue});
+             setList(newList);
+        }}>추가</button>
+        <ul>
+             {list.map((value) => {return <li key = {value.id}>{value.value}</li>})}
+        </ul>
+        </>
+    );
+};
 
 export default Map;
-
 /*
 [ map() ]
 - map() 의 인자로 넘겨지는 callback 함수를 실행한 결과를 가지고 새로운 배열을 만들 시 사용
