ccdc408019ef47543e1f6a3bfa09e0b6d98e5a3a
migyeonglee <ak10044@naver.com>
2022-10-13 16:00:37 +0900

    useCallback

--- first-app/src/HooksNext.js
+++ first-app/src/HooksNext.js
@@ -1,23 +1,33 @@
-import React,{useMemo, useRef, useState} from 'react';
+import React,{useCallback, useMemo, useRef, useState} from 'react';
 
 const HooksNext = () => {
     let [list, setState]=useState([]);
-    let [text, setTaxt]=useState("");
-    let addNumber = () =>{
+    let [text, setText]=useState("");
+    // let addNumber = () =>{
+    //     let newList = list.concat((Number)(inputNumber.current.value));
+    //     setState(newList);
+    // }
+    let inputNumber = useRef(null);
+    
+    let buttonClick = useCallback( () => {
+        setText("test");
+    },[]);
+    
+    let addNumber =useCallback( () =>{
         let newList = list.concat((Number)(inputNumber.current.value));
         setState(newList);
-    }
-    let inputNumber = useRef(null);
-
+    },[list]);
     let getAvg =()=>{
         if(list.length === 0) return 0;
         const sum = list.reduce((a, b)=> (a + b));
         return sum / list.length;
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     let Avg = useMemo(()=>{return getAvg()},[list])
     return (
         <>
-                <input ref={inputNumber}type="text" onChange={(e)=>{setTaxt(e.target.value)}} />
+                <input value={text} type="text" onChange={(e)=>{setText(e.target.value)}} />
+                <button onClick={buttonClick}>test로 변경</button>
             <div>
                 <input ref={inputNumber}type="number" />
                 <button onClick={addNumber}>추가</button>
