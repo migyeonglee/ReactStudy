309feafac3f70ad4067e0dc2aedf05e6d22a1254
migyeonglee <ak10044@naver.com>
2022-10-25 13:21:05 +0900

    redux start

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,27 +1,14 @@
-// import './app.scss';
+import './App.css';
 import React, { Component } from 'react';
-import {BrowserRouter} from "react-router-dom"
-// import {Nav,Content,Footer} from "react-router-dom"
-// import ReactRouter from './ReactRouter';
-import Nav from './Nav';
-import Content from './Content';
-import Footer from './Footer';
 
 class App extends Component {
 
   render() {
     return (
-      // <BrowserRouter>
       <>
-      <BrowserRouter>
-      <Nav/>
-      <Content/>
-      <Footer/>
-      {/* <ReactRouter/> */}
-      </BrowserRouter>
+     
 
       </>
-      // </BrowserRouter>
     );
   }
 }
